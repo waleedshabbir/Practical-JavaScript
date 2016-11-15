1e0c635d2f586c89ca39fa8564a8b653885175f2
Waleed Shabbir <waleedshabbir1@gmail.com>
2016-11-16 01:11:27 +0500

    Version 5 Completed

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 var todoList = {
   todos: [],
   displayTodos: function(){
-    console.log('My Todos: ', this.todos);
+      if(this.todos.length === 0){
+        console.log("Your TodoList is Empty");
+      }
+      else{
+      console.log('My Todos: ');  
+      for(var i=0;i<this.todos.length;i++){
+
+      if(this.todos[i].completed === true){
+              console.log('(x)', this.todos[i].todoText);
+
+      }
+      else{
+              console.log('( )', this.todos[i].todoText);
+
+      }
+      
+      }
+      
+    }
+    
+    
   },
   addTodo: function(todoText){
     this.todos.push({
