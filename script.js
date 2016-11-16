6f159cb4d39f85d14c0f77f10980f44363d1f695
Waleed Shabbir <waleedshabbir1@gmail.com>
2016-11-16 13:39:13 +0500

    Version 6 completed

--- script.js
+++ script.js
@@ -43,6 +43,33 @@ var todoList = {
     var todo = this.todos[position];
     todo.completed = !todo.completed;
     this.displayTodos();
+  },
+  toggleAll: function(){
+    var totalTodos = this.todos.length;
+    var completedTodos = 0;
+    
+    
+      for(var i=0;i<totalTodos;i++){
+      if(this.todos[i].completed === true){
+          completedTodos++;
+        }
+      }
+    
+    // Case 1 ALL FALSE
+    if(completedTodos === totalTodos){
+      for(var i=0;i<totalTodos;i++){
+        this.todos[i].completed = false;
+      }
+
+    }
+    else{
+      for(var i=0;i<totalTodos;i++){
+        this.todos[i].completed = true;
+      }
+    }
+    
+    this.displayTodos();
+    
   }
 };
 
