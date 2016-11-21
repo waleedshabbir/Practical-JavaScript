06745afaf9e620ad50fd68fa3ebb2c318d02dc1e
Waleed Shabbir <waleedshabbir1@gmail.com>
2016-11-22 00:49:40 +0500

    V8 - Getting Data From Input

--- script.js
+++ script.js
@@ -75,18 +75,41 @@ var todoList = {
 
 
 
-var displayTodosButton = document.getElementById('displayTodosButton');
-
-displayTodosButton.addEventListener('click',function(){
-  todoList.displayTodos();
-})
-
-
-var toggleAllButton = document.getElementById('toggleAllButton');
-
-toggleAllButton.addEventListener('click',function(){
-  todoList.toggleAll();
-})
+var handlers = {
+  displayTodos: function(){
+    todoList.displayTodos();
+  },
+  
+  toggleAll:function(){
+    todoList.toggleAll();
+  },
+  
+  addTodo:function(){
+   var addTodoTextInput = document.getElementById('addTodoTextInput');
+   todoList.addTodo(addTodoTextInput.value);
+   addTodoTextInput.value = '';
+  },
+  changeTodo:function(){
+    var changeTodoPositionInput = document.getElementById('changeTodoPositionInput');
+    var changeTodoTextInput = document.getElementById('changeTodoTextInput');
+    todoList.changeTodo(changeTodoPositionInput.valueAsNumber,changeTodoTextInput.value)
+    changeTodoPositionInput.value = '';
+    changeTodoTextInput.value = '';
+  },
+  
+  deleteTodo:function(){
+        var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
+        todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
+        deleteTodoPositionInput.value = '';
+  },
+  
+  toggleCompleted:function(){
+            var toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
+        todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
+        toggleCompletedPositionInput.value = '';
+  }
+  
+};
 
 
 
