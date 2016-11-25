58f813434b29a50884a6062912eec730f2539a26
Waleed Shabbir <waleedshabbir1@gmail.com>
2016-11-26 01:49:05 +0500

    Version 11 completed - forEach

--- script.js
+++ script.js
@@ -20,25 +20,22 @@ var todoList = {
     var totalTodos = this.todos.length;
     var completedTodos = 0;
     
-    // Get number of completed todos.
-    for (var i = 0; i < totalTodos; i++) {
-      if (this.todos[i].completed === true) {
-        completedTodos++;
-      }
-    }
+
+    this.todos.forEach(function(todo) {
+      if (todo.completed === true) {
+         completedTodos++;
+       }
+    });
     
-    // Case 1: If everything’s true, make everything false.
-    if (completedTodos === totalTodos) {
-      for (var i = 0; i < totalTodos; i++) {
-        this.todos[i].completed = false;
+    this.todos.forEach(function(todo){
+      if(completedTodos === totalTodos){
+        todo.completed = false;
       }
-    // Case 2: Otherwise, make everything true.
-    } else {
-      for (var i = 0; i < totalTodos; i++) {
-        this.todos[i].completed = true;
-      }      
-    }
-  }
+      else{
+        todo.completed = true;
+      }
+    });
+}
 };
 
 var handlers = {
@@ -76,23 +73,25 @@ var view = {
   displayTodos: function() {
     var todosUl = document.querySelector('ul');
     todosUl.innerHTML = '';
-    for (var i = 0; i < todoList.todos.length; i++) {
-      var todoLi = document.createElement('li');
-      var todo = todoList.todos[i];
-      var todoTextWithCompletion = '';
 
-      if (todo.completed === true) {
-        todoTextWithCompletion = '(x) ' + todo.todoText;
-      } else {
-        todoTextWithCompletion = '( ) ' + todo.todoText;
-      }
+    todoList.todos.forEach(function(todo,position){
+        var todoLi = document.createElement('li');
+   
+       var todoTextWithCompletion = '';
+
+       if (todo.completed === true) {
+         todoTextWithCompletion = '(x) ' + todo.todoText;
+       } else {
+         todoTextWithCompletion = '( ) ' + todo.todoText;
+       }
       
-      todoLi.id = i;
+       todoLi.id = position;
       
-      todoLi.textContent = todoTextWithCompletion;
-      todoLi.appendChild(this.createDeleteButton());
-      todosUl.appendChild(todoLi);
-    }
+       todoLi.textContent = todoTextWithCompletion;
+       todoLi.appendChild(this.createDeleteButton());
+       todosUl.appendChild(todoLi);
+    }, this);
+    
   },
   createDeleteButton: function(){
     var deleteButton = document.createElement('button');
